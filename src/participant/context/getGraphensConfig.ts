387f20bf08ccbcb5bc3a1b274ac290a94dda8736
Nikolai Dorofeev <contact@d0rich.me>
2026-06-24 14:21:52 +0200

    Parse YAML content in getGraphensConfig function

--- src/participant/context/getGraphensConfig.ts
+++ src/participant/context/getGraphensConfig.ts
@@ -1,9 +1,10 @@
 import { workspace } from 'vscode'
 import { GraphensConfig, GraphensConfigSchema } from '../../models/GraphensConfig'
+import * as yaml from 'yaml'
 
 export async function getGraphensConfig(): Promise<GraphensConfig | null> {
   const docs = await workspace.findFiles('.graphens/config.y{a,}ml', '**/node_modules/**', 1)
   if (!docs.length) return null
   const content = await workspace.openTextDocument(docs[0]!)
-  return GraphensConfigSchema.parse(content.getText())
+  return GraphensConfigSchema.parse(yaml.parse(content.getText()))
 }
