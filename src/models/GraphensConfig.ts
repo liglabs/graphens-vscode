38450fbf26d4d7841781aca6982580085e1b2988
Nikolai Dorofeev <contact@d0rich.me>
2026-06-25 15:18:32 +0200

    Add JSDoc comments for GraphensConfigSchema and GraphensConfig types

--- src/models/GraphensConfig.ts
+++ src/models/GraphensConfig.ts
@@ -1,7 +1,13 @@
 import {z} from 'zod'
 
+/**
+ * Represents the expected schema of the graphens config located in .graphens/config.yaml
+ */
 export const GraphensConfigSchema = z.object({
   sources: z.url().array()
 }).partial()
 
+/**
+ * Represents the expected type of the graphens config located in .graphens/config.yaml
+ */
 export type GraphensConfig = z.output<typeof GraphensConfigSchema>
