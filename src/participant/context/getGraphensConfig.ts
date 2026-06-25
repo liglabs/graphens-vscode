b0def7aba81362bca484cc4f80e488cc35610b94
Nikolai Dorofeev <contact@d0rich.me>
2026-06-25 15:20:31 +0200

    Add error handling and logging to getGraphensConfig function

--- src/participant/context/getGraphensConfig.ts
+++ src/participant/context/getGraphensConfig.ts
@@ -1,10 +1,16 @@
 import { workspace } from 'vscode'
 import { GraphensConfig, GraphensConfigSchema } from '../../models/GraphensConfig'
 import * as yaml from 'yaml'
+import logger from '../../logger'
 
 export async function getGraphensConfig(): Promise<GraphensConfig | null> {
   const docs = await workspace.findFiles('.graphens/config.y{a,}ml', '**/node_modules/**', 1)
   if (!docs.length) return null
-  const content = await workspace.openTextDocument(docs[0]!)
-  return GraphensConfigSchema.parse(yaml.parse(content.getText()))
+  try {
+    const content = await workspace.openTextDocument(docs[0]!)
+    return GraphensConfigSchema.parse(yaml.parse(content.getText()))
+  } catch (e) {
+    logger.error(e)
+    return null
+  }
 }
