8f8e4e19b77f0d12f458ef0b815d79e2f17f71bd
Nikolai Dorofeev <contact@d0rich.me>
2026-06-29 10:15:47 +0200

    Remove rag call, to replace with MCP later

--- src/participant/GraphensParticipant.ts
+++ src/participant/GraphensParticipant.ts
@@ -3,7 +3,6 @@ import BASE_PROMPT from '../messages/BASE_PROMPT.md?raw'
 import RESPONSE_TO_CHEATER from '../messages/RESPONSE_TO_CHEATER.md?raw'
 import { getHistory } from './context/utils/getHistory'
 import { isCheating } from './guards/cheating'
-import { getCourseContent } from './context/utils/getCourseContent'
 import { getFilesByLink } from './context/utils/getFilesByLink'
 import { processDebugCommands } from '../utils/processDebugCommands'
 import { getSessionKey } from '../utils/getSessionKey'
@@ -55,20 +54,17 @@ export class GraphensParticipant {
       graphensContext,
       workspaceContext,
       errorsContext,
-      courseContent,
       mentionedFiles,
     ] = await Promise.all([
       getReadmeContextMessage(),
       getGraphensContextMessage(cache, (e) => stream.markdown('$(error) Erreur en lisant `.graphens/config.yaml`')),
       getWorkspaceContextMessage(),
       getErrorsContextMessages(ctx, cache),
-      getCourseContent(request.prompt),
       getFilesByLink(request.prompt),
     ])
 
     const prompt = [
-      'Voici le contenu du cours pertinent: \n\n',
-      ...courseContent.map((chunk) => `## ${chunk.titre} \n\n ${chunk.texte}`),
+      // TODO: Add MCP support
       "Voici les fichiers mentionnés par l'étudiant",
       ...mentionedFiles.map(
         (file) => `### ${file.original}\n\n${file.content}`,
