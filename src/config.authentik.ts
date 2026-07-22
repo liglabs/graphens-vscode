58576c10279295a62d114249fcf1bbab50573450
Nikolai Dorofeev <contact@d0rich.me>
2026-07-22 08:39:38 +0200

    Authentification provider draft

---
+++ src/config.authentik.ts
@@ -0,0 +1,8 @@
+export const authentikConfig = {
+  // address: 'https://im2ag-ens-l3m.univ-grenoble-alpes.fr', // production
+  address: 'http://localhost', // local
+  redirectUri: 'vscode://graphens.graphens-vscode/oauth2/authentik/callback',
+  // redirectUri: 'https://im2ag-ens-l3m.univ-grenoble-alpes.fr/',
+  // clientId: 'axGrghIENUz9dSp19GiQtSvgcFLbJkxyqu4oKhVw', // production
+  clientId: 'd4CdvvkyvJbTi48QbPoYmKJrzBeqdCrjxU2rOa3P', // local
+}
